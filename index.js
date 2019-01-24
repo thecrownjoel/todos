72896354b701f926ac31bb7a26e9866c801039ba
Unknown <joel@45press.com>
2019-01-24 16:07:31 -0500

    Need committ
    adding load window

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
+
+let mainWindow;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
 });
