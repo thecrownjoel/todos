6d2eb4757d01ae502dc16ecc86acdee4070c36b8
Unknown <joel@45press.com>
2019-01-24 23:51:10 -0500

    Adding file menus

--- index.js
+++ index.js
@@ -7,10 +7,17 @@ let mainWindow;
 app.on('ready', () => {
     mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 });
 
 const menuTemplate = [
     {
-        
+        label: 'File',
+        submenu: [
+            { label: 'New Todo' }
+        ]
+
     }
-]
+];
