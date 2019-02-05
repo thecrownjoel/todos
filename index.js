ee0c69ad4cadf87e1b2a137cf3369961282a6702
Unknown <joel@45press.com>
2019-02-04 22:23:05 -0500

    add todo menu

--- index.js
+++ index.js
@@ -12,14 +12,26 @@ app.on('ready', () => {
     Menu.setApplicationMenu(mainMenu);
 });
 
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add New Todo'
+    });
+    addWindow.loadURL(`file://${__dirname}/add.html`);
+}
+
 const menuTemplate = [
     {
         label: 'File',
         submenu: [
-            { label: 'New Todo' },
+            { 
+                label: 'New Todo',
+                click() { createAddWindow(); } 
+            },
             {
                 label: 'Quit',
-                accelerator: process.platform ==,
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
                 click() {
                     app.quit();
                 }
