b3fdb81a221113c6d037fa075e315badf58cecd1
Unknown <joel@45press.com>
2019-02-06 17:14:10 -0500

    more work on file menu

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ let mainWindow;
 app.on('ready', () => {
     mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/main.html`);
+    mainWindow.on('closed', () => app.quit());
 
     const mainMenu = Menu.buildFromTemplate(menuTemplate);
     Menu.setApplicationMenu(mainMenu);
